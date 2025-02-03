dd733c2a850d2e20a99a4eb65edbcb8ea3c27af0
Jesper Morais <jeppe.morris@hotmail.se>
2025-02-03 15:50:21 +0100

    added simple local UDP recv

--- server/main.go
+++ server/main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
-	"fmt"	
+	"fmt"
+	"net"
+)
+
+const (
+	port = "1234"
 )
 
 func main() {	
-	fmt.Println("Hello, World!")
+
+	udpAddr, err := net.ResolveUDPAddr("udp", port)
+
+	if(err != nil) {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	conn, err := net.ListenUDP("udp", udpAddr)
+	
+	if(err != nil) {
+		fmt.Println("Error: ", err)
+		return
+	}
+	for {
+		var buf [512]byte
+		_, addr, err := conn.ReadFromUDP(buf[0:])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Print("> ", string(buf[0:]))
+
+		// Write back the message over UPD
+		conn.WriteToUDP([]byte("Hello UDP Client\n"), addr)
+	}
+
 }
